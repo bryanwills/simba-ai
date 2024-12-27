ae5ba03fafadd194e6eae48195d10a9a07bd6fd8
zeroualihamid <networksystemsmaroc@gmail.com>
2024-12-27 21:01:35 +0100

    save work

--- services/agents/__init__.py
+++
@@ -1,8 +0,0 @@
-# agents/__init__.py
-import os
-import glob
-
-# Automatically import all Python files in the agents directory
-modules = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
-__all__ = [os.path.basename(f)[:-3] for f in modules if os.path.isfile(f) and not f.endswith('__init__.py')]
-# __all__ = ["RetrievalAgent", "GraderAgent"]
