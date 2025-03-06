684d51b897d0d84e0cd66d22914eecaefed1caa0
Hamza Zerouali <zeroualihamza0206@gmail.com>
2025-03-06 15:39:06 +0100

    feat: refacto retrieval, added abstract class and modulari ty & modify parameters from config file

--- simba/retrieval/__init__.py
+++ simba/retrieval/__init__.py
@@ -1,3 +1,23 @@
-from .retriever import RetrievalMethod, Retriever
+"""
+Retrieval module for document retrieval.
+"""
+from simba.retrieval.retriever import Retriever
+from simba.retrieval.base import BaseRetriever, RetrievalMethod
+from simba.retrieval.default import DefaultRetriever
+from simba.retrieval.semantic import SemanticRetriever
+from simba.retrieval.keyword import KeywordRetriever
+from simba.retrieval.ensemble import EnsembleSearchRetriever
+from simba.retrieval.hybrid import HybridRetriever
+from simba.retrieval.factory import RetrieverFactory
 
-__all__ = ["Retriever", "RetrievalMethod"]
+__all__ = [
+    "Retriever",
+    "BaseRetriever",
+    "DefaultRetriever",
+    "SemanticRetriever",
+    "KeywordRetriever",
+    "EnsembleSearchRetriever",
+    "HybridRetriever",
+    "RetrieverFactory",
+    "RetrievalMethod"
+]
